b7fee8d2cad3c50f025bbb83636c9ebecaad4f0b
Josh Greco <greco.joshua@gmail.com>
2018-03-09 12:10:42 -0600

    Updating...

--- src/HospitalProject/HospitalApp.java
+++ src/HospitalProject/HospitalApp.java
@@ -4,9 +4,11 @@ public class HospitalApp {
 
 	public static void main(String[] args) {
 		
-		Doctor phil = new Doctor (1111, "Phill", "Brain");
+		Doctor phil = new Doctor (1111, "Phil", "Brain");
+		Nurse donna = new Nurse (1112, "dDonna", 3);
 		
 		System.out.println(phil);
+		System.out.println(donna);
 
 	}
 
