e1c57dde8249a0a106fb48b8b1f8b994a6b65282
Josh Greco <greco.joshua@gmail.com>
2018-03-11 20:53:27 -0600

    Beginning of the compare demo

---
+++ src/SortingAList.java
@@ -0,0 +1,32 @@
+
+public class SortingAList {
+
+	public static void main(String[] args) {
+		
+		//This was for showing the declaration of a String
+		//String name = "Greco";
+		
+		
+		String name1 = "Jules";
+		String name2 = "Greco";
+		
+		//This organizes by the first letter of the String: 
+		//J is 8 letters before R
+		//Does it return negative, positive , or zero
+		System.out.println("V1.0 = " + name2.compareTo(name1));
+		
+		
+		//Goes to the first different letter
+		String name3 = "Smitty";
+		String name4 = "Smith";
+		
+		System.out.println("V1.2 = " + name3.compareTo(name4));
+		
+		
+		
+		
+		
+
+	}
+
+}
