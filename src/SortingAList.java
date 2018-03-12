6bd515c848d871d5ffd38bc39b3dbf2992c0ad2c
Josh Greco <greco.joshua@gmail.com>
2018-03-11 21:03:10 -0600

    Finished the demo for Sorting

--- src/SortingAList.java
+++ src/SortingAList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class SortingAList {
 
@@ -7,22 +9,34 @@ public class SortingAList {
 		//String name = "Greco";
 		
 		
-		String name1 = "Jules";
-		String name2 = "Greco";
+//		String name1 = "Jules";
+//		String name2 = "Greco";
+//		
+//		//This organizes by the first letter of the String: 
+//		//J is 8 letters before R
+//		//Does it return negative, positive , or zero
+//		System.out.println("V1.0 = " + name2.compareTo(name1));
+//		
+//		
+//		//Goes to the first different letter
+//		String name3 = "Smitty";
+//		String name4 = "Smith";
+//		
+//		System.out.println("V1.2 = " + name3.compareTo(name4));
 		
-		//This organizes by the first letter of the String: 
-		//J is 8 letters before R
-		//Does it return negative, positive , or zero
-		System.out.println("V1.0 = " + name2.compareTo(name1));
 		
+		ArrayList<String> names =  new ArrayList();
 		
-		//Goes to the first different letter
-		String name3 = "Smitty";
-		String name4 = "Smith";
+		names.add("Tom");
+		names.add("Adam");
+		names.add("John");
 		
-		System.out.println("V1.2 = " + name3.compareTo(name4));
+		//one line to put in alphabetical order
+		Collections.sort(names);
 		
-		
+		for(String name: names) {
+			System.out.println(name);
+		}
 		
 		
 		
