136dd9e5557202b5440fbf46b35284cc34287603
Josh Greco <greco.joshua@gmail.com>
2018-03-18 23:08:13 -0400

    Minor bug fixes

--- src/FRQ2008Num4/YummyChecker.java
+++ src/FRQ2008Num4/YummyChecker.java
@@ -2,4 +2,8 @@ package FRQ2008Num4;
 
 public class YummyChecker {
 
+	protected Object aChecker;
+	protected Object kChecker;
+	Checker yummyChecker = new AndChecker(new NotChecker(aChecker), new NotChecker(kChecker));
+	
 }
